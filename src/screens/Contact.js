32efb350f1314889ad236efc393abb1abc426eb0
achjauhari123 <achjauhari027@gmail.com>
2023-11-13 15:16:51 +0700

    coba

---
+++ src/screens/Contact.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from 'react';
+
+
+const Contact = () => {
+    return (
+        <View>
+            <Text>contact</Text>
+        </View>
+    );
+}
+
+export default Contact;
